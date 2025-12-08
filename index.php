2eaae31c4638f36acc25086ae869cb4efc33306a
Samuel <Semnfumuntoto@hotmail.com>
2025-12-08 20:55:35 +0100

    Add initial login and registration pages
    Created index.php for login, register.php for user registration, and added empty function.php and style.css files to set up the basic structure for authentication pages.

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    <link stylesheet="stylesheet" href="style.css"> 
+</head>
+<body>
+    <section>
+        <h1> Login form </h1>
+            <form>
+                <label for="fname">Username:</label> <br>
+                 <input type="text" id="uname" name="uname"><br><br>
+                 <label for="fname">Password:</label> <br>
+                 <input type="password" id="pname" name="pname"><br><br>
+                 <a href="register.php">No account? Register Here.</a>
+            <form> <br> 
+            <button>
+                Login
+            </button>
+    </section>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register page</title>
+</head>
+<body>
+    <section>
+        <h1>Registration Form</h1>
+        <label for="fname">First Name:</label> <br>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="fname">Last Name:</label> <br>
+        <input type="text" id="lname" name="lname"><br><br>
+    </section>
+</body>
+</html>

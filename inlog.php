149537fc37b2e7a9201959b66fb65dc3ffe8ad7c
Samuel 3noch <114405673+Samuel3noch@users.noreply.github.com>
2025-12-18 14:51:27 +0100

    Update login form placeholders and input constraints
    A fast update for the login page

--- inlog.php
+++ inlog.php
@@ -15,11 +15,11 @@
             <form action="" method="POST">
                      <!-- Voer hier uw gebruikersnaam in. -->
                 <label for="uName">Username:</label> <br>
-                <input type="text" id="userName" name="userName" minlength="6" maxlength="64" required><br><br>
+                <input type="text" id="userName" name="userName" placeholder="Your Username" minlength="4" maxlength="32" required><br><br>
 
                     <!-- Voer hier uw wachtwoord in. -->
                 <label for="password">Password:</label> <br>
-                <input type="password" id="userpassword" name="password" minlength="6" maxlength="64" required><br><br>
+                <input type="password" id="userpassword" name="password" placeholder="Your Password" minlength="4" maxlength="32" required><br><br>
 
                     <!-- een link om naar de register page te gaan. -->
                 <a href="register.php">No account? Register Here.</a> <br><br>

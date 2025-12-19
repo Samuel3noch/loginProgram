a153cbc03fe98f51eedc224f7e1fffe1205f7261
Samuel <Semnfumuntoto@hotmail.com>
2025-12-19 16:25:32 +0100

    Refactor login and register pages, update styles
    Moved inlog.php and register.php to the content directory and improved their forms with placeholders and updated field names. Updated navigation to include a Home link and linked the stylesheet correctly. Enhanced style.css with new color variables and added comments for better structure.

--- content/inlog.php
+++ content/inlog.php
@@ -4,22 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-   <!-- <link rel="stylesheet" href="style.css"> -->
+   <link rel="stylesheet" href="/loginProgram/style.css">
 </head>
 <body>
     <nav> 
-        
+        <h1><a href="/loginProgram/index.php">Home</a></h1>
     </nav>
-    <section class="sectionForm">
+    <section>
         <h1> Login form </h1>
             <form action="" method="POST">
                      <!-- Voer hier uw gebruikersnaam in. -->
                 <label for="uName">Username:</label> <br>
-                <input type="text" id="userName" name="userName" minlength="6" maxlength="64" required><br><br>
+                <input type="text" id="userNameInlog" name="userName" minlength="6" maxlength="64" placeholder="Your Username" required><br><br>
 
                     <!-- Voer hier uw wachtwoord in. -->
                 <label for="password">Password:</label> <br>
-                <input type="password" id="userpassword" name="password" minlength="6" maxlength="64" required><br><br>
+                <input type="password" id="userpassword" name="password" minlength="6" maxlength="64" placeholder="Your Password" required><br><br>
 
                     <!-- een link om naar de register page te gaan. -->
                 <a href="register.php">No account? Register Here.</a> <br><br>
--- content/register.php
+++ content/register.php
@@ -4,38 +4,38 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register page</title>
-   <!-- <link rel="stylesheet" href="style.css"> -->
+   <link rel="stylesheet" href="/loginProgram/style.css">
 </head>
 <body>
     <nav> 
-        
+        <h1><a href="/loginProgram/index.php">Home</a></h1>
     </nav>
-    <section class="sectionForm">
+    <section>
         <h1>Registration Form</h1>
             <form action="" method="POST">
                     <!-- Voer hier uw voornaam in. -->
                 <label for="fName">First Name:</label> <br>
-                <input type="text" id="fName" name="firstName" required><br><br>
+                <input type="text" id="fName" name="firstName" placeholder="Your Firstname" required><br><br>
 
                     <!-- Voer hier uw achternaam in. -->
                 <label for="lName">Last Name:</label> <br>
-                <input type="text" id="lName" name="lastName" required><br><br>
+                <input type="text" id="lName" name="lastName" placeholder="Your Lastname" required><br><br>
 
                     <!-- Voer hier uw telefoon nummer in. -->
                 <label for="cNumber">Contact Number:</label> <br>
-                <input type="tel" id="cNumber" name="contactNumber" required> <br><br>
+                <input type="tel" id="cNumber" name="contactNumber" placeholder="Your ContactNumber" required> <br><br>
                 
                     <!-- Voer hier uw email in. -->
                 <label for="email">Email:</label> <br>
-                <input type="email" id="email" name="email" required> <br><br>
+                <input type="email" id="email" name="email" placeholder="Your E-mail" required> <br><br>
 
                     <!-- Voer hier uw gebruikersnaam in. -->
                 <label for="uName">Username:</label> <br>
-                <input type="text" id="uName" name="userName" minlength="6" maxlength="64" required><br><br>
+                <input type="text" id="uName" name="userNameRegister" minlength="6" maxlength="64" placeholder="Your Username" required><br><br>
 
                      <!-- Voer hier uw wachtwoord in. -->
                 <label for="password">Password:</label> <br>
-                <input type="password" id="password" name="password" minlength="6" maxlength="64" required><br><br>
+                <input type="password" id="password" name="password" minlength="6" maxlength="64" placeholder="Your Password" required><br><br>
                     <!-- een link om naar de inlog page. -->
                 <a href="inlog.php"><--Back</a> <br><br>
                 
